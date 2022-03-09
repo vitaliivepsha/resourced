f0dd2ea5c2e412430fff4e6984c37f63abc0257b
vitaliivepsha <vitaly.vepsha@gmail.com>
2022-03-09 17:53:14 +0200

    first commit

---
+++ app/modules/slider/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var $ = require('jquery');
+require('slick-carousel');
+require('../../../node_modules/slick-carousel/slick/slick.scss');
+
+module.exports = function () {
+    $('.slick-slider').each(function () {
+        var $this = $(this);
+        if ($this.parents('.hidden').length == 0) {
+            $this.slick();
+        }
+    });
+
+
+};
