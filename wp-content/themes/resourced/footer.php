0ac61ed842803d37b1a7cc77ee1ba2570a22083e
OlgaBesginova <bezginova.olga@gmail.com>
2022-04-14 15:45:11 +0300

    acf

--- wp-content/themes/resourced/footer.php
+++ wp-content/themes/resourced/footer.php
@@ -1,1 +1,133 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-xs-6">
+                <div class="footer-left">
+                    <div class="footer-logo">
+                        <img src="<?php echo get_template_directory_uri() ?>/images/pixel.jpg" data-original="<?php echo get_template_directory_uri() ?>/images/logo-footer.svg" class="lazy" alt="">
+                    </div>
+                    <div class="footer-social">
+                        <a href="javascript:void(0)">
+                            <svg width="11" height="20" viewBox="0 0 11 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M11 0H8C6.67392 0 5.40215 0.526784 4.46447 1.46447C3.52678 2.40215 3 3.67392 3 5V8H0V12H3V20H7V12H10L11 8H7V5C7 4.73478 7.10536 4.48043 7.29289 4.29289C7.48043 4.10536 7.73478 4 8 4H11V0Z"
+                                    fill="white" />
+                            </svg>
+
+                        </a>
+                        <a href="javascript:void(0)">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M16 8C17.5913 8 19.1174 8.63214 20.2426 9.75736C21.3679 10.8826 22 12.4087 22 14V21H18V14C18 13.4696 17.7893 12.9609 17.4142 12.5858C17.0391 12.2107 16.5304 12 16 12C15.4696 12 14.9609 12.2107 14.5858 12.5858C14.2107 12.9609 14 13.4696 14 14V21H10V14C10 12.4087 10.6321 10.8826 11.7574 9.75736C12.8826 8.63214 14.4087 8 16 8V8Z"
+                                    fill="white" />
+                                <path d="M6 9H2V21H6V9Z" fill="white" />
+                                <path
+                                    d="M4 6C5.10457 6 6 5.10457 6 4C6 2.89543 5.10457 2 4 2C2.89543 2 2 2.89543 2 4C2 5.10457 2.89543 6 4 6Z"
+                                    fill="white" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+
+                        </a>
+                    </div>
+                    <span>© 2022 Resourced, AB.</span>
+                    <span>Made with love in Sweden</span>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6">
+                <div class="footer-col">
+                    <span>Product</span>
+                    <ul>
+                        <li><a href="javascript:void(0)">Resource by Resourced™</a></li>
+                        <li><a href="javascript:void(0)">Pricing</a></li>
+                        <li><a href="javascript:void(0)">Supplier Database</a></li>
+                        <li><a href="javascript:void(0)">Quomoda</a></li>
+                        <li><a href="javascript:void(0)">Tariff Identifer</a></li>
+                        <li><a href="javascript:void(0)">Knowledge bank</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6">
+                <div class="footer-col">
+                    <span>Who we are</span>
+                    <ul>
+                        <li><a href="javascript:void(0)">About</a></li>
+                        <li><a href="javascript:void(0)">Careers</a></li>
+                        <li><a href="javascript:void(0)">Partners</a></li>
+                        <li><a href="javascript:void(0)">Privacy Policy</a></li>
+                        <li><a href="javascript:void(0)">Terms & Conditions</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6">
+                <div class="footer-col">
+                    <span>Get in touch</span>
+                    <ul>
+                        <li><a href="javascript:void(0)">Join newsletter</a></li>
+                        <li><a href="javascript:void(0)">Contact us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<div class="mfp-hide">
+    <div class="signup-popup mfp-with-anim" id="signup-popup">
+        <button title="Close (Esc)" type="button" class="mfp-close">
+            <svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M11 0.785797L1 9.94557" stroke="#DFDFDF" />
+                <path d="M11.011 9.93563L0.989281 0.795776" stroke="#DFDFDF" />
+            </svg>
+        </button>
+        <span class="signup-popup__title">We´re soon there!</span>
+        <span class="signup-popup__subtitle">just making some final touches</span>
+        <div class="signup-popup__descr">
+            <p>We will gradually let new brands in to ensure a good onboarding.</p>
+            <p>To get access please let us your details below and we´ll get in touch</p>
+        </div>
+        <div class="signup-popup__main">
+            <div class="signup-popup__left">
+                <div class="signup-popup__info">
+                    <div class="signup-popup__img">
+                        <picture>
+                            <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.webp" data-original="<?php echo get_template_directory_uri() ?>/images/popup-pic.webp" class="lazy-web"
+                                    type="image/webp">
+                            <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/popup-pic.png" class="lazy-web"
+                                    type="image/png">
+                            <img src="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/popup-pic.png" class="lazy" alt="">
+                        </picture>
+                    </div>
+                    <span class="signup-popup__info-text">Ready to fall in love with picking colors?</span>
+                </div>
+            </div>
+            <form class="signup-popup__form ajax_form" data-error-title="Error!" data-error-message="Try again later"
+                  data-success-title="Thank you!" data-success-message="We´ll get back to you with an update shortly">
+                <div class="signup-popup__form-inner">
+                    <div class="input-wrapper">
+                        <label>Your Name</label>
+                        <input class="input" name="name" placeholder="Elias Gröndal" data-title="Name"
+                               data-validate-required="Required!">
+                    </div>
+                    <div class="input-wrapper">
+                        <label>Work email</label>
+                        <input class="input" name="email" placeholder="elias@resourced.com" data-title="Email"
+                               data-validate-required="Required!" data-validate-email="Please use valid e-mail adress">
+                    </div>
+                    <div class="input-wrapper">
+                        <label>Phone number</label>
+                        <input class="input" name="phone" placeholder="+46 739 597929" data-title="Phone"
+                               data-validate-required="Required!">
+                    </div>
+                    <button type="submit">Sign me up</button>
+                </div>
+            </form>
+        </div>
+        <div class="signup-popup__footnote">By pressing “Sign me up” you agree to our <a href="javascript:void(0)">terms &
+                conditions</a></div>
+    </div>
+</div>
+
+
 <?php wp_footer(); ?>
+</body>
+
+</html>
--- wp-content/themes/resourced/header.php
+++ wp-content/themes/resourced/header.php
@@ -1,1 +1,276 @@
-<?php wp_head(); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex">
+    <title>RESOURCED</title>
+    <link rel="icon" href="<?php echo get_template_directory_uri() ?>/images/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() ?>/images/favicon.png" type="image/png">
+    <meta name="theme-color" content="#10084d">
+    <meta name="msapplication-navbutton-color" content="#10084d">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#10084d">
+    <?php wp_head(); ?>
+</head>
+
+<body>
+<header class="header">
+    <div class="header-main">
+        <div class="header-main-inner">
+            <div class="header-main-logo">
+                <a href="javascript:void(0)">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/logo.svg" class="lazy" alt="">
+                </a>
+            </div>
+            <div class="header-main-nav">
+                <ul>
+                    <!-- <li class="current"> -->
+                    <li>
+                        <a href="javascript:void(0)">Solution</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)">Pricing</a>
+                    </li>
+                    <li class="has-children">
+                        <a href="javascript:void(0)">Resources</a>
+                        <div class="drop-menu">
+                            <div class="drop-menu-item first">
+                                <span>Footwear</span>
+                                <ul>
+                                    <li>
+                                        <div>
+                                            <picture>
+                                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.webp" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-01.webp"
+                                                        class="lazy-web" type="image/webp">
+                                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-01.png"
+                                                        class="lazy-web" type="image/png">
+                                                <img src="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-01.png" class="lazy"
+                                                     alt="">
+                                            </picture>
+                                        </div>
+                                        <a href="javascript:void(0)">
+                                            <b>Source Suppliers</b>
+                                            <span>Submit a request</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <picture>
+                                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.webp" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-02.webp"
+                                                        class="lazy-web" type="image/webp">
+                                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-02.png"
+                                                        class="lazy-web" type="image/png">
+                                                <img src="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-02.png" class="lazy"
+                                                     alt="">
+                                            </picture>
+                                        </div>
+                                        <a href="javascript:void(0)">
+                                            <b>Supplier Database</b>
+                                            <span>Search among 1900+ suppliers</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <picture>
+                                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.webp" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-03.webp"
+                                                        class="lazy-web" type="image/webp">
+                                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-03.png"
+                                                        class="lazy-web" type="image/png">
+                                                <img src="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-03.png" class="lazy"
+                                                     alt="">
+                                            </picture>
+                                        </div>
+                                        <a href="javascript:void(0)">
+                                            <b>Tariff Identifier</b>
+                                            <span>The smooth way to get HS codes</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="drop-menu-item second">
+                                <span>Learning</span>
+                                <ul>
+                                    <li>
+                                        <div>
+                                            <picture>
+                                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.webp" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-04.webp"
+                                                        class="lazy-web" type="image/webp">
+                                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-04.png"
+                                                        class="lazy-web" type="image/png">
+                                                <img src="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-04.png" class="lazy"
+                                                     alt="">
+                                            </picture>
+                                        </div>
+                                        <a href="javascript:void(0)">
+                                            <b>Quomoda</b>
+                                            <span>The online school for fashion</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <picture>
+                                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.webp" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-05.webp"
+                                                        class="lazy-web" type="image/webp">
+                                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-05.png"
+                                                        class="lazy-web" type="image/png">
+                                                <img src="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-05.png" class="lazy"
+                                                     alt="">
+                                            </picture>
+                                        </div>
+                                        <a href="javascript:void(0)">
+                                            <b>Knowledge Bank</b>
+                                            <span>Dive deep into footwear terminology</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <picture>
+                                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.webp" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-06.webp"
+                                                        class="lazy-web" type="image/webp">
+                                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-06.png"
+                                                        class="lazy-web" type="image/png">
+                                                <img src="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-06.png" class="lazy"
+                                                     alt="">
+                                            </picture>
+                                        </div>
+                                        <a href="javascript:void(0)">
+                                            <b>Innovative materials</b>
+                                            <span>A small but helpful guide</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="header-main-control">
+                <a href="javascript:void(0)" class="popup-btn" data-mfp-src="#signup-popup" data-effect="mfp-zoom-in">Sign up</a>
+            </div>
+        </div>
+    </div>
+    <div class="mobile-btn">
+        <span></span>
+    </div>
+</header>
+<div class="mobile-menu__wrapper">
+    <ul class="mobile-menu">
+        <li>
+            <a href="javascript:void(0)">Solution</a>
+        </li>
+        <li>
+            <a href="javascript:void(0)">Pricing</a>
+        </li>
+        <li class="has-children">
+            <a href="javascript:void(0)">Resources</a>
+            <span>
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+              d="M5.57805 10.9798L10.6278 15.4915C11.3882 16.1708 12.6166 16.1708 13.377 15.4915L18.4268 10.9798C19.6551 9.88233 18.7777 8.001 17.0424 8.001H6.94286C5.2076 8.001 4.34972 9.88233 5.57805 10.9798Z"
+              fill="#671DFF" />
+        </svg>
+      </span>
+            <div class="submenu">
+                <span>Footwear</span>
+                <ul>
+                    <li>
+                        <div>
+                            <picture>
+                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.webp" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-01.webp"
+                                        class="lazy-web" type="image/webp">
+                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-01.png" class="lazy-web"
+                                        type="image/png">
+                                <img src="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-01.png" class="lazy" alt="">
+                            </picture>
+                        </div>
+                        <a href="javascript:void(0)">
+                            <b>Source Suppliers</b>
+                            <span>Submit a request</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div>
+                            <picture>
+                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.webp" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-02.webp"
+                                        class="lazy-web" type="image/webp">
+                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-02.png" class="lazy-web"
+                                        type="image/png">
+                                <img src="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-02.png" class="lazy" alt="">
+                            </picture>
+                        </div>
+                        <a href="javascript:void(0)">
+                            <b>Supplier Database</b>
+                            <span>Search among 1900+ suppliers</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div>
+                            <picture>
+                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.webp" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-03.webp"
+                                        class="lazy-web" type="image/webp">
+                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-03.png" class="lazy-web"
+                                        type="image/png">
+                                <img src="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-03.png" class="lazy" alt="">
+                            </picture>
+                        </div>
+                        <a href="javascript:void(0)">
+                            <b>Tariff Identifier</b>
+                            <span>The smooth way to get HS codes</span>
+                        </a>
+                    </li>
+                </ul>
+                <span>Learning</span>
+                <ul>
+                    <li>
+                        <div>
+                            <picture>
+                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.webp" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-04.webp"
+                                        class="lazy-web" type="image/webp">
+                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-04.png" class="lazy-web"
+                                        type="image/png">
+                                <img src="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-04.png" class="lazy" alt="">
+                            </picture>
+                        </div>
+                        <a href="javascript:void(0)">
+                            <b>Quomoda</b>
+                            <span>The online school for fashion</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div>
+                            <picture>
+                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.webp" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-05.webp"
+                                        class="lazy-web" type="image/webp">
+                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-05.png" class="lazy-web"
+                                        type="image/png">
+                                <img src="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-05.png" class="lazy" alt="">
+                            </picture>
+                        </div>
+                        <a href="javascript:void(0)">
+                            <b>Knowledge Bank</b>
+                            <span>Dive deep into footwear terminology</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div>
+                            <picture>
+                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.webp" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-06.webp"
+                                        class="lazy-web" type="image/webp">
+                                <source srcset="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-06.png" class="lazy-web"
+                                        type="image/png">
+                                <img src="<?php echo get_template_directory_uri() ?>/images/pixel.png" data-original="<?php echo get_template_directory_uri() ?>/images/icons/drop-06.png" class="lazy" alt="">
+                            </picture>
+                        </div>
+                        <a href="javascript:void(0)">
+                            <b>Innovative materials</b>
+                            <span>A small but helpful guide</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+    <span class="mobile-signup popup-btn" data-mfp-src="#signup-popup" data-effect="mfp-zoom-in">Sign up</span>
+</div>
