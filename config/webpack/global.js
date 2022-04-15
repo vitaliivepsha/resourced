28c7e7fa8844dce3ceea7c16ae18ff2f9ef62802
Artem <nicktlibra@gmail.com>
2022-04-15 15:15:50 +0300

    правки главной страницы

--- config/webpack/global.js
+++ config/webpack/global.js
@@ -19,229 +19,229 @@ var AssetInjectHtmlWebpackPlugin = require('../../app/modules/asset-inject-html-
  * @param  {[type]} _path [description]
  * @return {[type]}       [description]
  */
-module.exports = function(_path, ENV) {
-  // define local variables
-  var dependencies = Object.keys(require(_path + '/package').dependencies);
-  var rootAssetPath = _path + 'app';
+module.exports = function (_path, ENV) {
+    // define local variables
+    var dependencies = Object.keys(require(_path + '/package').dependencies);
+    var rootAssetPath = _path + 'app';
 
-  // return objecy
-  var webpackConfig = {
-    // Точки входа
-    entry: {
-      application: _path + '/app/app.js',
-      vendors: dependencies
-    },
-
-    // Файлы вывода
-    output: {
-      path: path.join(_path, 'dist'),
-      chunkFilename: '[id].bundle.[chunkhash].js',
-      publicPath: ab == 'b' ? ab_dir : '/'
-    },
-
-    // resolves modules
-    resolve: {
-      extensions: ['', '.js'],
-      modulesDirectories: ['node_modules'],
-      alias: {
-        _svg: path.join(_path, 'app', 'assets', 'svg'),
-        _fonts: path.join(_path, 'app', 'assets', 'fonts'),
-        _modules: path.join(_path, 'app', 'modules'),
-        _images: path.join(_path, 'app', 'assets', 'images'),
-        _stylesheets: path.join(_path, 'app', 'assets', 'stylesheets'),
-        _templates: path.join(_path, 'app', 'assets', 'templates')
-      }
-    },
-
-    // modules resolvers
-    module: {
-      loaders: [
-        {
-          test: /\.html$/,
-          loaders: [
-            'html-loader?attrs=source:srcset link:href img:src img:data-src picture:data-src div:data-src a:data-mfp-src source:data-original div:data-mfp-src img:data-original img:scrset source:src img:data-lazy image:href li:data-src div:data-src&interpolate=1',
-            'purifycss-loader'
-          ]
+    // return objecy
+    var webpackConfig = {
+        // Точки входа
+        entry: {
+            application: _path + '/app/app.js',
+            vendors: dependencies
         },
-        {
-          loader: 'babel',
-          test: /\.js$/,
-          query: {
-            presets: ['es2015'],
-            ignore: ['node_modules', 'bower_components']
-          }
-        }
-      ]
-    },
 
-    // post css
-    postcss: [autoprefixer({ browsers: ['last 5 versions'] })],
+        // Файлы вывода
+        output: {
+            path: path.join(_path, 'dist'),
+            chunkFilename: '[id].bundle.[chunkhash].js',
+            publicPath: ab == 'b' ? ab_dir : '/'
+        },
 
-    sassLoader: {
-      outputStyle: 'expanded',
-      sourceMap: 'true'
-    },
+        // resolves modules
+        resolve: {
+            extensions: ['', '.js'],
+            modulesDirectories: ['node_modules'],
+            alias: {
+                _svg: path.join(_path, 'app', 'assets', 'svg'),
+                _fonts: path.join(_path, 'app', 'assets', 'fonts'),
+                _modules: path.join(_path, 'app', 'modules'),
+                _images: path.join(_path, 'app', 'assets', 'images'),
+                _stylesheets: path.join(_path, 'app', 'assets', 'stylesheets'),
+                _templates: path.join(_path, 'app', 'assets', 'templates')
+            }
+        },
 
-    // load plugins
-    plugins: [
-      new webpack.optimize.CommonsChunkPlugin(
-        'vendors',
-        'assets/js/vendors.[hash].js'
-      ),
-      new TextPlugin('assets/css/[name].[chunkhash].css'),
-      new Manifest(path.join(_path + '/config', 'manifest.json'), {
-        rootAssetPath: rootAssetPath,
-        ignorePaths: ['.DS_Store']
-      }),
-      new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery'
-      }),
-      // new AssetInjectHtmlWebpackPlugin({
-      //   texts: {
-      //     ab: ENV == "production" && ab == "a" ? "<?php if((!isset($_COOKIE['AB']) && rand(0, 1)) || (isset($_COOKIE['AB']) && $_COOKIE['AB'] == 2)) { SetCookie('AB','2',time()+3600*24*365); $ref = $_SERVER['QUERY_STRING']; if ($ref != '') $ref = '?' . $ref; header('HTTP/1.1 301 Moved Permanently'); header('Location:http://"+domain+ab_dir+"' . $ref); exit();  }else{ SetCookie('AB','1',time()+3600*24*365); } ?>" : " ",
-      //     utm: ENV == 'production' ? '<?=!empty($_GET["utm_campaign"])?str_replace(array("_poisk", "_kms", "-poisk_g", "-kms"), "", $_GET["utm_campaign"]):""?>' : ' '
-      //   }
-      // }),
-      new webpack.DllReferencePlugin({
-        context: __dirname,
-        manifest: require('../../app/vendor/vendor-manifest.json')
-      })
-    ]
-  };
+        // modules resolvers
+        module: {
+            loaders: [
+                {
+                    test: /\.html$/,
+                    loaders: [
+                        'html-loader?attrs=source:srcset link:href img:src img:data-src picture:data-src div:data-src a:data-mfp-src source:data-original div:data-mfp-src img:data-original img:scrset source:src img:data-lazy image:href li:data-src div:data-src&interpolate=1',
+                        'purifycss-loader'
+                    ]
+                },
+                {
+                    loader: 'babel',
+                    test: /\.js$/,
+                    query: {
+                        presets: ['es2015'],
+                        ignore: ['node_modules', 'bower_components']
+                    }
+                }
+            ]
+        },
 
-  if (ENV == 'production') {
-    // webpackConfig.devtool = "nosources-source-map";
-    webpackConfig.devtool = 'source-map';
+        // post css
+        postcss: [autoprefixer({ browsers: ['last 5 versions'] })],
 
-    webpackConfig.plugins[webpackConfig.plugins.length] = new TextPlugin(
-      'assets/css/[name].[chunkhash].css'
-    );
+        sassLoader: {
+            outputStyle: 'expanded',
+            sourceMap: 'true'
+        },
 
-    webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
-      test: /\.scss$/,
-      loader: TextPlugin.extract('style', 'css!sass!')
+        // load plugins
+        plugins: [
+            new webpack.optimize.CommonsChunkPlugin(
+                'vendors',
+                'assets/js/vendors.[hash].js'
+            ),
+            new TextPlugin('assets/css/[name].[chunkhash].css'),
+            new Manifest(path.join(_path + '/config', 'manifest.json'), {
+                rootAssetPath: rootAssetPath,
+                ignorePaths: ['.DS_Store']
+            }),
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery'
+            }),
+            // new AssetInjectHtmlWebpackPlugin({
+            //   texts: {
+            //     ab: ENV == "production" && ab == "a" ? "<?php if((!isset($_COOKIE['AB']) && rand(0, 1)) || (isset($_COOKIE['AB']) && $_COOKIE['AB'] == 2)) { SetCookie('AB','2',time()+3600*24*365); $ref = $_SERVER['QUERY_STRING']; if ($ref != '') $ref = '?' . $ref; header('HTTP/1.1 301 Moved Permanently'); header('Location:http://"+domain+ab_dir+"' . $ref); exit();  }else{ SetCookie('AB','1',time()+3600*24*365); } ?>" : " ",
+            //     utm: ENV == 'production' ? '<?=!empty($_GET["utm_campaign"])?str_replace(array("_poisk", "_kms", "-poisk_g", "-kms"), "", $_GET["utm_campaign"]):""?>' : ' '
+            //   }
+            // }),
+            new webpack.DllReferencePlugin({
+                context: __dirname,
+                manifest: require('../../app/vendor/vendor-manifest.json')
+            })
+        ]
     };
 
-    webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
-      test: /\.(css|ico|png|webp)$/i,
-      loaders: [
-        'url-loader?limit=4096&context=' +
-          rootAssetPath +
-          '&name=assets/static/[ext]/[name]_[hash].[ext]'
-      ]
-    };
+    if (ENV == 'production') {
+        // webpackConfig.devtool = "nosources-source-map";
+        webpackConfig.devtool = 'source-map';
 
-    webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
-      test: /\.woff(2)?(\?[a-z0-9=&.]+)?$/i,
-      loaders: [
-        'url-loader?limit=4096&context=' +
-          rootAssetPath +
-          '&name=assets/static/[ext]/[name]_[hash].[ext]'
-      ]
-    };
+        webpackConfig.plugins[webpackConfig.plugins.length] = new TextPlugin(
+            'assets/css/[name].[chunkhash].css'
+        );
 
-    webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
-      test: /\.(ttf|eot)(\?[a-z0-9=&.]+)?$/i,
-      loaders: [
-        'url-loader?limit=4096&context=' +
-          rootAssetPath +
-          '&name=assets/static/[ext]/[name]_[hash].[ext]'
-      ]
-    };
+        webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
+            test: /\.scss$/,
+            loader: TextPlugin.extract('style', 'css!sass!')
+        };
 
-    webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
-      test: /\.svg(\?[a-z0-9=&.]+)?$/i,
-      loaders: [
-        'url-loader?limit=4096&context=' +
-          rootAssetPath +
-          '&name=assets/static/[ext]/[name]_[hash].[ext]',
-        'image-webpack-loader?{optimizationLevel: 8, interlaced: false, pngquant:{quality: "80-90", speed: 4}, mozjpeg: {quality: 80}}'
-      ]
-    };
+        webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
+            test: /\.(css|ico|png|webp)$/i,
+            loaders: [
+                'url-loader?limit=4096&context=' +
+                rootAssetPath +
+                '&name=assets/static/[ext]/[name]_[hash].[ext]'
+            ]
+        };
 
-    webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
-      test: /\.(gif|jpe?g)$/i,
-      loaders: [
-        'url-loader?limit=4096&context=' +
-          rootAssetPath +
-          '&name=assets/static/[ext]/[name]_[hash].[ext]',
-        'image-webpack-loader?{optimizationLevel: 8, interlaced: false, pngquant:{quality: "80-90", speed: 4}, mozjpeg: {quality: 80}}'
-      ]
-    };
-  } else {
-    webpackConfig.cache = true;
-    webpackConfig.devtool = 'eval';
+        webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
+            test: /\.woff(2)?(\?[a-z0-9=&.]+)?$/i,
+            loaders: [
+                'url-loader?limit=4096&context=' +
+                rootAssetPath +
+                '&name=assets/static/[ext]/[name]_[hash].[ext]'
+            ]
+        };
 
-    webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
-      test: /\.scss$/,
-      loaders: ['style', 'css?sourceMap', 'sass?sourceMap']
-    };
+        webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
+            test: /\.(ttf|eot)(\?[a-z0-9=&.]+)?$/i,
+            loaders: [
+                'url-loader?limit=4096&context=' +
+                rootAssetPath +
+                '&name=assets/static/[ext]/[name]_[hash].[ext]'
+            ]
+        };
 
-    webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
-      test: /\.(css|ico|png|jpg|jpeg|gif|webp)$/i,
-      loaders: [
-        'url-loader?limit=4096&context=' +
-          rootAssetPath +
-          '&name=assets/static/[ext]/[name]_[hash].[ext]'
-      ]
-    };
+        webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
+            test: /\.svg(\?[a-z0-9=&.]+)?$/i,
+            loaders: [
+                'url-loader?limit=4096&context=' +
+                rootAssetPath +
+                '&name=assets/static/[ext]/[name]_[hash].[ext]',
+                'image-webpack-loader?{optimizationLevel: 8, interlaced: false, pngquant:{quality: "80-90", speed: 4}, mozjpeg: {quality: 80}}'
+            ]
+        };
 
-    webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
-      test: /\.woff(2)?(\?[a-z0-9=&.]+)?$/i,
-      loaders: [
-        'url-loader?limit=4096&context=' +
-          rootAssetPath +
-          '&name=assets/static/[ext]/[name]_[hash].[ext]'
-      ]
-    };
+        webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
+            test: /\.(gif|jpe?g)$/i,
+            loaders: [
+                'url-loader?limit=4096&context=' +
+                rootAssetPath +
+                '&name=assets/static/[ext]/[name]_[hash].[ext]',
+                'image-webpack-loader?{optimizationLevel: 8, interlaced: false, pngquant:{quality: "80-90", speed: 4}, mozjpeg: {quality: 80}}'
+            ]
+        };
+    } else {
+        webpackConfig.cache = true;
+        webpackConfig.devtool = 'eval';
 
-    webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
-      test: /\.(ttf|eot|svg)(\?[a-z0-9=&.]+)?$/i,
-      loaders: [
-        'url-loader?limit=4096&context=' +
-          rootAssetPath +
-          '&name=assets/static/[ext]/[name]_[hash].[ext]'
-      ]
-    };
+        webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
+            test: /\.scss$/,
+            loaders: ['style', 'css?sourceMap', 'sass?sourceMap']
+        };
 
-    webpackConfig.plugins[
-      webpackConfig.plugins.length
-    ] = new webpack.HotModuleReplacementPlugin();
+        webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
+            test: /\.(css|ico|png|jpg|jpeg|gif|webp|mp4)$/i,
+            loaders: [
+                'url-loader?limit=4096&context=' +
+                rootAssetPath +
+                '&name=assets/static/[ext]/[name]_[hash].[ext]'
+            ]
+        };
 
-    webpackConfig.output.publicPath = 'http://localhost:88/';
-  }
+        webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
+            test: /\.woff(2)?(\?[a-z0-9=&.]+)?$/i,
+            loaders: [
+                'url-loader?limit=4096&context=' +
+                rootAssetPath +
+                '&name=assets/static/[ext]/[name]_[hash].[ext]'
+            ]
+        };
 
-  var templates = glob.sync(_path + '/app/assets/templates/layouts/*.html');
+        webpackConfig.module.loaders[webpackConfig.module.loaders.length] = {
+            test: /\.(ttf|eot|svg)(\?[a-z0-9=&.]+)?$/i,
+            loaders: [
+                'url-loader?limit=4096&context=' +
+                rootAssetPath +
+                '&name=assets/static/[ext]/[name]_[hash].[ext]'
+            ]
+        };
 
-  for (var temp in templates) {
-    var template = templates[temp].replace(
-      path
-        .join(_path, 'app', 'assets', 'templates', 'layouts/')
-        .replace(/\\/g, '/'),
-      ''
-    );
-    if(template == 'index.html'){
-        var destination = template;
-    }else{
-        var destination = (template.replace('.html', ''))+'/index.html';
+        webpackConfig.plugins[
+            webpackConfig.plugins.length
+        ] = new webpack.HotModuleReplacementPlugin();
+
+        webpackConfig.output.publicPath = 'http://localhost:88/';
     }
 
-    webpackConfig.plugins[webpackConfig.plugins.length] = new HtmlPlugin({
-      title: 'Landing',
-      chunks: ['application', 'vendors'],
-      filename: destination,
-      template: path.join(
-        _path,
-        'app',
-        'assets',
-        'templates',
-        'layouts',
-        template
-      )
-    });
-  }
+    var templates = glob.sync(_path + '/app/assets/templates/layouts/*.html');
 
-  return webpackConfig;
+    for (var temp in templates) {
+        var template = templates[temp].replace(
+            path
+                .join(_path, 'app', 'assets', 'templates', 'layouts/')
+                .replace(/\\/g, '/'),
+            ''
+        );
+        if (template == 'index.html') {
+            var destination = template;
+        } else {
+            var destination = (template.replace('.html', '')) + '/index.html';
+        }
+
+        webpackConfig.plugins[webpackConfig.plugins.length] = new HtmlPlugin({
+            title: 'Landing',
+            chunks: ['application', 'vendors'],
+            filename: destination,
+            template: path.join(
+                _path,
+                'app',
+                'assets',
+                'templates',
+                'layouts',
+                template
+            )
+        });
+    }
+
+    return webpackConfig;
 };
