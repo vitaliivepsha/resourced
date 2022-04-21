04acf38b48ab3991ee6e1215b578ec9936be0074
OlgaBezginova <bezginova.olga@gmail.com>
2022-04-21 12:26:57 +0300

    footer

--- wp-content/themes/resourced/footer.php
+++ wp-content/themes/resourced/footer.php
@@ -13,15 +13,17 @@
                         <?php endif; ?>
                     </div>
                     <div class="footer-social">
-                        <a href="javascript:void(0)">
+                        <?php if(!empty(get_field('facebook', 'option'))): ?>
+                        <a href="<?php the_field('facebook', 'option'); ?>">
                             <svg width="11" height="20" viewBox="0 0 11 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                     d="M11 0H8C6.67392 0 5.40215 0.526784 4.46447 1.46447C3.52678 2.40215 3 3.67392 3 5V8H0V12H3V20H7V12H10L11 8H7V5C7 4.73478 7.10536 4.48043 7.29289 4.29289C7.48043 4.10536 7.73478 4 8 4H11V0Z"
                                     fill="white" />
                             </svg>
-
                         </a>
-                        <a href="javascript:void(0)">
+                        <?php endif; ?>
+                        <?php if(!empty(get_field('linkedin', 'option'))): ?>
+                        <a href="<?php the_field('linkedin', 'option'); ?>">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                     d="M16 8C17.5913 8 19.1174 8.63214 20.2426 9.75736C21.3679 10.8826 22 12.4087 22 14V21H18V14C18 13.4696 17.7893 12.9609 17.4142 12.5858C17.0391 12.2107 16.5304 12 16 12C15.4696 12 14.9609 12.2107 14.5858 12.5858C14.2107 12.9609 14 13.4696 14 14V21H10V14C10 12.4087 10.6321 10.8826 11.7574 9.75736C12.8826 8.63214 14.4087 8 16 8V8Z"
@@ -32,6 +34,7 @@
                                     fill="white" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
                         </a>
+                        <?php endif; ?>
                     </div>
                     <span>© <?php echo date('Y'); ?> <?php echo get_field('copyright', 'option')['line1']; ?></span>
                     <span><?php echo get_field('copyright', 'option')['line2']; ?></span>
